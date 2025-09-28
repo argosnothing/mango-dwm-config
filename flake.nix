28c7e886143a5bf2397b85645d634441a75ee72b
salivala <argosnothing@gmail.com>
2025-09-27 23:03:59 -0400

    I added mango as an input

--- flake.nix
+++ flake.nix
@@ -3,21 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    mango.url = "github:DreamMaoMao/mango";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
+  outputs = {nixpkgs, ...} @ inputs: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
   in {
     devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.wmenu
-          pkgs.waybar
-          pkgs.st
-        ];
+      buildInputs = [
+        pkgs.wmenu
+        pkgs.waybar
+        pkgs.st
+      ];
+      packages = [
+        inputs.mango.packages.${system}.mango
+      ];
       shellHook = ''
         export PS1="(mango-dwm) $PS1"
         export OLDHOME=$HOME
