9bd0fbfb6243f59e90ab5c3e763458a0b9d70434
salivala <argosnothing@gmail.com>
2025-09-27 22:28:55 -0400

    i hate it

--- flake.nix
+++ flake.nix
@@ -20,7 +20,8 @@
         ];
       shellHook = ''
         export PS1="(mango-dwm) $PS1"
-        export GIT_CONFIG_GLOBAL="$HOME/.config/git/config"
+        export OLDHOME=$HOME
+        export GIT_CONFIG_GLOBAL="$OLDHOME/.config/git/config"
         export XDG_CONFIG_HOME=$PWD
         export MANGOCONFIG=$PWD
         export HOME=$PWD #idk blame waybar
